5055a905dcfe43bf98668ea2b53c199477e99a12
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-14 14:29:57 +0200

    Aggiungere i file di progetto.

---
+++ CibiPreferiti/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Nome repo: csharp-cibi-preferiti
+Create un progetto App Console CibiPreferiti con Visual Studio.
+Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). 
+L’array deve essere già inizializzato nel programma ordinato già con i cibi dal più preferito al meno preferito, 
+e i vostri cibi preferiti non vanno chiesti all’utente tramite input (hard-coded).
+Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+La lunghezza della classifica
+La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+Il vostro cibo top (prima posizione della classifica)
+Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+Cercate per ogni richiesta che viene fatta di preparare le rispettive funzioni  e di usarle nel vostro programma principale!
+Le modalità di consegna sono le solite: repo GitHub, commit e push
+BONUS
+Chiedere all'utente quanti cibi preferiti ha, e farglieli inserite da input della Console nell'array.
+Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. 
+Potrebbe essere interessante creare una funzione chiamata indiciMedianaArray(int lunghezzaArray)
+che vi restituisce l'indice della posizione mediana (che restituisca gli indici delle eventuali posizioni mediani)
+e poi fare una funzione (StampaArrayMediana) che si occupa di fare tutto il lavoro con  la funzione precedente
+e richiedendo direttamente l'intero array. Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica. 
+ */
+
