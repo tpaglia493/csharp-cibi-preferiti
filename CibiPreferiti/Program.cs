433d57d507a557077a7c41bd78965f94f9f9449a
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-14 15:24:28 +0200

    added functions to display elements in the first and last postion of the array

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,8 +1,5 @@
 ﻿
 /*Nome repo: csharp-cibi-preferiti
-
-La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
-Il vostro cibo top (prima posizione della classifica)
 Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
 Cercate per ogni richiesta che viene fatta di preparare le rispettive funzioni  e di usarle nel vostro programma principale!
 Le modalità di consegna sono le solite: repo GitHub, commit e push
@@ -23,7 +20,12 @@ string[] FavouriteFoods = { "Crépes", "Carbonara", "Risotto alla crema di scamp
 DisplayArrayLength(FavouriteFoods);
 
 Console.WriteLine("La classifica è la seguente:");
+
 DisplayArrayElementsByIndex(FavouriteFoods);
+
+DisplayArrayFirstPosition(FavouriteFoods);
+
+DisplayArrayLastPosition(FavouriteFoods);
 
 
 //---------------------- FUNZIONI -----------------------------
@@ -32,7 +34,7 @@ DisplayArrayElementsByIndex(FavouriteFoods);
 
 void DisplayArrayLength(string[] array)
 { int ArrayLength = array.Length;
-    Console.WriteLine($"Nella lista ci sono {ArrayLength} elementi");
+    Console.WriteLine($"There are {ArrayLength} elements in the list");
 }
 
 //FUNZIONE PER STAMAPARE GLI ELEMENTI DELL'ARRAY IN ORDINE DI INDICE CRESCENTE
@@ -40,6 +42,19 @@ void DisplayArrayElementsByIndex(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine($"Al {i+1}° posto: {array[i]}");
+        Console.WriteLine($"{i+1}) {array[i]}");
     }
+}
+
+//FUNZIONE PER STAMPARE LA PRIMA POSIZIONE DELL'ARRAY
+void DisplayArrayFirstPosition(string[] array)
+{
+    Console.WriteLine($"The first position in the array is:{array[0]}");
+}
+
+//FUNZIONE PER STAMPARE L'ULTIMA POSIZIONE DELL'ARRAY
+void DisplayArrayLastPosition(string[] array)
+{
+    int ArrayLastPosition = array.Length - 1;
+    Console.WriteLine($"The last position in the array is:{array[ArrayLastPosition]}");
 }
